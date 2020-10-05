0c9640e425f6e931a2227d57c1b511fcc6a20535
Joel <joel.t.norman@gmail.com>
2020-10-04 20:13:46 -0400

    Added XUnit

---
+++ visit-canton-coders/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace visit_canton_coders
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
