766aa1b061680dc1d66a367ba4408b877186f9ba
Joel <joel.t.norman@gmail.com>
2020-10-04 20:25:13 -0400

    Added Fluent Assert and Updated.

--- visit-canton-coders-tests/UnitTest1.cs
+++ visit-canton-coders-tests/UnitTest1.cs
@@ -6,9 +6,13 @@ namespace visit_canton_coders_tests
     /*
      * The Shortest Path Algorithm.
 
-        Canton Coders are located around the world. For 2021, we would like to visit every coder IRL. To do this, we must determine the shortest path between each community member's house. Keep in mind, when a community member lives with another community member, it ten times more valuable to visit those community members. The distance between each members is measured in minutes, and the goal is to visit the most community members in 30 calendar days.
+        Canton Coders are located around the world. For 2021, we would like to visit every coder IRL. To do this, we must determine the shortest path between each community member's house. 
+        Keep in mind, when a community member lives with another community member, it ten times more valuable to visit those community members. 
+        The distance between each members is measured in minutes, 
+        and the goal is to visit the most community members in 30 calendar days.
 
-        You must write a piece of software that can read from 2 CSVs and will output a route for the most valuable travel route. The input files are as follows:
+        You must write a piece of software that can read from 2 CSVs and will output a route for the most valuable travel route. 
+        The input files are as follows:
 
         members.csv (name,location)
         locations (currentlocation,connectinglocation,timeoftravel)
@@ -21,7 +25,7 @@ namespace visit_canton_coders_tests
         [Fact]
         public void Test1()
         {
-
+            
         }
     }
 }
